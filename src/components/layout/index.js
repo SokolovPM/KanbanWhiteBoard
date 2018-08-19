669f0af792daa392ab50bcf5caa9d6846a0967ea
Petr Sokolov <tenebrise@mail.ru>
2018-08-19 19:54:56 +0200

    add border like it is whiteboard

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -8,7 +8,15 @@ import { logout, backToTheProjectList } from '../../actions';
 
 import { LogoutButton, HomeButton } from './header-buttons';
 
-
+const WhiteBoard = styled.div`
+  background-color: #f2f2f2;
+  position: relative;
+  border-radius: 5px;
+  height: 100%;
+  border: 10px solid #adb2bd;
+  box-shadow: inset -1px 2px 2px #404040, 6px 9px 1px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+`;
 const Container = styled.div`
   max-width: 1280px;
   margin: 20px auto;
@@ -31,25 +39,27 @@ class Layout extends Component {
   render() {
     const { auth, children, logout, backToTheProjectList, location } = this.props;
     return (
-      <Container>
-        {auth &&
-          <Header>
-            {location.pathname === '/' ?
-              <div />
-              :
-              <HomeButton callback={backToTheProjectList} />
-            }
-            <LogoutButton callback={logout} />
-          </Header>
-        }
-        <Content>
-          {auth ?
-            <div>{children}</div>
-            :
-            <LoginPage />
+      <WhiteBoard>
+        <Container>
+          {auth &&
+            <Header>
+              {location.pathname === '/' ?
+                <div />
+                :
+                <HomeButton callback={backToTheProjectList} />
+              }
+              <LogoutButton callback={logout} />
+            </Header>
           }
-        </Content>
-      </Container>
+          <Content>
+            {auth ?
+              <div>{children}</div>
+              :
+              <LoginPage />
+            }
+          </Content>
+        </Container>
+      </WhiteBoard>
     )
   }
 }
